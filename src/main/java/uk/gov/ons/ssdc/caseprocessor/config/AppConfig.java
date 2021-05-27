1fd3cf99b6d6b65e0ea6ec052e67f1a29e1e252e
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-05-27 17:39:22 +0100

    Add unit tests, integration tests, fix healthcheck

--- src/main/java/uk/gov/ons/ssdc/caseprocessor/config/AppConfig.java
+++ src/main/java/uk/gov/ons/ssdc/caseprocessor/config/AppConfig.java
@@ -3,8 +3,10 @@ package uk.gov.ons.ssdc.caseprocessor.config;
 import java.util.TimeZone;
 import javax.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
+@EnableScheduling
 public class AppConfig {
 
   @PostConstruct
