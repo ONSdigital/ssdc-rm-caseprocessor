039d73daf5d4f253e68fa919934c4a9e793ee8b8
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-06-14 11:40:11 +0100

    Add sample staging DB tables & link to collex

---
+++ src/main/java/uk/gov/ons/ssdc/caseprocessor/model/entity/BulkProcess.java
@@ -0,0 +1,5 @@
+package uk.gov.ons.ssdc.caseprocessor.model.entity;
+
+public enum BulkProcess {
+  SAMPLE
+}
---
+++ src/main/java/uk/gov/ons/ssdc/caseprocessor/model/entity/JobRowStatus.java
@@ -0,0 +1,7 @@
+package uk.gov.ons.ssdc.caseprocessor.model.entity;
+
+public enum JobRowStatus {
+  STAGED,
+  PROCESSED_OK,
+  PROCESSED_ERROR
+}
