13536de9d75c80be4fe01f50a13818c5c53bc87e
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-06-24 12:53:11 +0100

    Update FulfilmentSchedulerTest.java

--- src/test/java/uk/gov/ons/ssdc/caseprocessor/schedule/FulfilmentSchedulerTest.java
+++ src/test/java/uk/gov/ons/ssdc/caseprocessor/schedule/FulfilmentSchedulerTest.java
@@ -3,6 +3,8 @@ package uk.gov.ons.ssdc.caseprocessor.schedule;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 import java.time.OffsetDateTime;
@@ -48,4 +50,23 @@ public class FulfilmentSchedulerTest {
     assertThat(fulfilmentNextTrigger.getTriggerDateTime()).isEqualTo(dateTimeNow.plusDays(1));
   }
 
+  @Test
+  public void testScheduleFulfilmentsNothingToDo() {
+    // Given
+    FulfilmentNextTrigger fulfilmentNextTrigger = new FulfilmentNextTrigger();
+    OffsetDateTime dateTimeNow = OffsetDateTime.now();
+    fulfilmentNextTrigger.setTriggerDateTime(dateTimeNow);
+
+    when(clusterLeaderManager.isThisHostClusterLeader()).thenReturn(true);
+    when(fulfilmentNextTriggerRepository.findByTriggerDateTimeBefore(any(OffsetDateTime.class))).thenReturn(
+        Optional.empty());
+
+    // When
+    underTest.scheduleFulfilments();
+
+    // Then
+    verifyNoInteractions(fulfilmentProcessor);
+    verify(fulfilmentNextTriggerRepository).findByTriggerDateTimeBefore(any(OffsetDateTime.class));
+    verifyNoMoreInteractions(fulfilmentNextTriggerRepository);
+  }
 }
