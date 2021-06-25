9f0398da33a23a7271d26fd0678748b6b323f7c4
LukeLoze1 <44841627+LukeLoze1@users.noreply.github.com>
2021-06-25 07:38:53 +0100

    Update src/test/java/uk/gov/ons/ssdc/caseprocessor/testutils/TestConstants.java
    Co-authored-by: Nick Grant <41681172+NickGrantONS@users.noreply.github.com>

--- src/test/java/uk/gov/ons/ssdc/caseprocessor/testutils/TestConstants.java
+++ src/test/java/uk/gov/ons/ssdc/caseprocessor/testutils/TestConstants.java
@@ -2,5 +2,5 @@ package uk.gov.ons.ssdc.caseprocessor.testutils;
 
 public class TestConstants {
   public static final String OUTBOUND_UAC_QUEUE = "caseProcessor.rh.uacUpdate";
-  public static final String OUTBOUND_CASE_QUEUE = "caseProcessor.rh.case";
+  public static final String OUTBOUND_CASE_QUEUE = "caseProcessor.rh.caseUpdate";
 }
