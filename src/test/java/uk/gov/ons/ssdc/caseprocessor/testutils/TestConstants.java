803605e2f9d2926836c12e493b58007e8b75c986
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2021-06-25 08:18:59 +0100

    more tinkering

--- src/test/java/uk/gov/ons/ssdc/caseprocessor/testutils/TestConstants.java
+++ src/test/java/uk/gov/ons/ssdc/caseprocessor/testutils/TestConstants.java
@@ -1,6 +1,6 @@
 package uk.gov.ons.ssdc.caseprocessor.testutils;
 
 public class TestConstants {
-  public static final String OUTBOUND_UAC_QUEUE = "caseProcessor.rh.uacUpdate";
-  public static final String OUTBOUND_CASE_QUEUE = "caseProcessor.rh.caseUpdate";
+  public static final String OUTBOUND_UAC_QUEUE = "events.rh.uacUpdate";
+  public static final String OUTBOUND_CASE_QUEUE = "events.rh.caseUpdate";
 }
