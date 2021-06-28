03254eaca2a5ef4141cf7eeb4f427b32f7095062
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-06-28 10:43:30 +0100

    Remove garbage import from pom

--- src/main/java/uk/gov/ons/ssdc/caseprocessor/model/repository/FulfilmentNextTriggerRepository.java
+++ src/main/java/uk/gov/ons/ssdc/caseprocessor/model/repository/FulfilmentNextTriggerRepository.java
@@ -4,10 +4,8 @@ import java.time.OffsetDateTime;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import uk.gov.ons.ssdc.caseprocessor.model.entity.FulfilmentNextTrigger;
 
-@RepositoryRestResource(exported = false)
 public interface FulfilmentNextTriggerRepository
     extends JpaRepository<FulfilmentNextTrigger, UUID> {
   Optional<FulfilmentNextTrigger> findByTriggerDateTimeBefore(OffsetDateTime triggerDateTime);
