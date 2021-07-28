41211b2665e9879762941496542e66c6b6d901bf
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-07-28 12:57:44 +0100

    Simplify and improve leader election

--- src/main/java/uk/gov/ons/ssdc/caseprocessor/schedule/ClusterLeaderManager.java
+++ src/main/java/uk/gov/ons/ssdc/caseprocessor/schedule/ClusterLeaderManager.java
@@ -7,73 +7,44 @@ import java.net.UnknownHostException;
 import java.time.OffsetDateTime;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.ReentrantLock;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.TransactionSynchronization;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
 import uk.gov.ons.ssdc.caseprocessor.model.entity.ClusterLeader;
 import uk.gov.ons.ssdc.caseprocessor.model.repository.ClusterLeaderRepository;
 
 @Component
 public class ClusterLeaderManager {
   private static final Logger log = LoggerFactory.getLogger(ClusterLeaderManager.class);
-  private static final UUID LEADER_ID = UUID.fromString("e469807b-f2e2-47bd-acf6-74f8943ff3db");
-  private static final ReentrantLock lock = new ReentrantLock();
+  protected static final UUID LEADER_ID = UUID.fromString("e469807b-f2e2-47bd-acf6-74f8943ff3db");
 
   private final ClusterLeaderRepository clusterLeaderRepository;
+  private final ClusterLeaderManagerStartup clusterLeaderManagerStartup;
 
   @Value("${scheduler.leaderDeathTimeout}")
   private int leaderDeathTimeout;
 
   private String hostName = InetAddress.getLocalHost().getHostName();
 
-  public ClusterLeaderManager(ClusterLeaderRepository clusterLeaderRepository)
+  public ClusterLeaderManager(
+      ClusterLeaderRepository clusterLeaderRepository,
+      ClusterLeaderManagerStartup clusterLeaderManagerStartup)
       throws UnknownHostException {
     this.clusterLeaderRepository = clusterLeaderRepository;
+    this.clusterLeaderManagerStartup = clusterLeaderManagerStartup;
   }
 
   @Transactional(isolation = Isolation.REPEATABLE_READ)
   public boolean isThisHostClusterLeader() {
-    if (!clusterLeaderRepository.existsById(LEADER_ID)) {
-      boolean isLockAcquired;
-
-      // Obtain a lock, so that only one thread can possibly ever be attempting to declare that
-      // this host is the leader, by recording it in the database
+    synchronized (LEADER_ID) {
       try {
-        isLockAcquired = lock.tryLock(10, TimeUnit.SECONDS);
-      } catch (InterruptedException e) {
-        throw new RuntimeException("Failed to get lock before thread interrupted");
+        clusterLeaderManagerStartup.doStartupChecksAndAttemptToElectLeaderIfRequired(LEADER_ID);
+      } catch (DataIntegrityViolationException dataIntegrityViolationException) {
+        // Multiple processes had a dead-heat when trying to become leader, so let the losers fail
+        return false;
       }
-
-      // If we couldn't obtain a lock then we can't continue... throw an error and let spring retry
-      if (!isLockAcquired) {
-        throw new RuntimeException("Failed to get lock before timeout");
-      }
-
-      // Now we are locked, record that this host is the cluster leader in the database
-      ClusterLeader clusterLeader = new ClusterLeader();
-      clusterLeader.setId(LEADER_ID);
-      clusterLeader.setHostName(hostName);
-      clusterLeader.setHostLastSeenAliveAt(OffsetDateTime.now());
-      clusterLeaderRepository.saveAndFlush(clusterLeader);
-
-      // Finally, only release the lock once the transaction has been committed (or rolled back)
-      if (TransactionSynchronizationManager.isSynchronizationActive()) {
-        TransactionSynchronizationManager.registerSynchronization(
-            new TransactionSynchronization() {
-              @Override
-              public void afterCompletion(int status) {
-                lock.unlock();
-              }
-            });
-      }
-
-      log.with("hostName", hostName)
-          .debug("No leader existed in DB, so this host is attempting to become leader");
     }
 
     Optional<ClusterLeader> lockedClusterLeaderOpt =
