f542e944a15d63b16de081a6fe8ea8d8e9a16013
Nick Grant <41681172+NickGrantONS@users.noreply.github.com>
2021-08-05 16:51:52 +0100

    Fix unreliable HealthCheckIT (#40)

--- src/main/java/uk/gov/ons/ssdc/caseprocessor/healthcheck/HeathCheck.java
+++ src/main/java/uk/gov/ons/ssdc/caseprocessor/healthcheck/HeathCheck.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -27,7 +27,7 @@ public class HeathCheck {
     clusterLeaderManager.leaderKeepAlive();
 
     Path path = Paths.get(fileName);
-    LocalDateTime now = LocalDateTime.now();
+    OffsetDateTime now = OffsetDateTime.now();
 
     try (BufferedWriter writer = Files.newBufferedWriter(path)) {
       writer.write(now.toString());
