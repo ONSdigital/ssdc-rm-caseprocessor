90d34e9edc55e2fa4989ce8df9f6ae0328cf01f6
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-08-11 12:08:25 +0100

    Update ExceptionManagerClient.java

--- src/main/java/uk/gov/ons/ssdc/caseprocessor/client/ExceptionManagerClient.java
+++ src/main/java/uk/gov/ons/ssdc/caseprocessor/client/ExceptionManagerClient.java
@@ -31,7 +31,7 @@ public class ExceptionManagerClient {
 
     ExceptionReport exceptionReport = new ExceptionReport();
     exceptionReport.setExceptionClass(cause.getClass().getName());
-    exceptionReport.setExceptionMessage("stoopid message"); // TODO
+    exceptionReport.setExceptionMessage("Deprecated: See stack trace root cause instead");
     exceptionReport.setExceptionRootCause(stackTraceRootCause);
     exceptionReport.setMessageHash(messageHash);
     exceptionReport.setService(service);
