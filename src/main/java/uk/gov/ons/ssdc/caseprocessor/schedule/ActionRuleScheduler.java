62def09bf95db622168a0f2fc57ec125b53eb694
LukeLoze1 <Luke.Loze@ext.ons.gov.uk>
2021-12-15 07:28:28 +0000

    remove temp test code

--- src/main/java/uk/gov/ons/ssdc/caseprocessor/schedule/ActionRuleScheduler.java
+++ src/main/java/uk/gov/ons/ssdc/caseprocessor/schedule/ActionRuleScheduler.java
@@ -4,9 +4,6 @@ import com.godaddy.logging.Logger;
 import com.godaddy.logging.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import uk.gov.ons.ssdc.common.model.entity.Case;
-
-import java.util.UUID;
 
 @Service
 public class ActionRuleScheduler {
@@ -22,13 +19,6 @@ public class ActionRuleScheduler {
 
   @Scheduled(fixedDelayString = "${scheduler.frequency}")
   public void triggerActionRule() {
-
-    Case caze = new Case();
-    caze.setId(UUID.randomUUID());
-    caze.setCaseRef(2324L);
-
-    log.with("Case: ", caze).info("Testing, testing 123");
-
     if (!clusterLeaderManager.isThisHostClusterLeader()) {
       return; // This host (i.e. pod) is not the leader... don't do any scheduling
     }
