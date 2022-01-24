a91f68fc4df4a1b031fa47eaea168d8b9341c80c
LukeLoze1 <Luke.Loze@ext.ons.gov.uk>
2022-01-24 09:56:22 +0000

    more play

--- src/main/java/uk/gov/ons/ssdc/caseprocessor/enrichment/CaseMetaDataGeneric.java
+++ src/main/java/uk/gov/ons/ssdc/caseprocessor/enrichment/CaseMetaDataGeneric.java
@@ -15,6 +15,8 @@ public class CaseMetaDataGeneric {
 //    This would be sorted by next scheduled task,  e.g. scheduledTasks[0]
 //    This should be doable with the wonder of Java8+ scheduleTasks = scheduledTasks.filter().sort(date).asec().  etc?
 //    We'd do a performance test to see if this was plausible, definitely prefereable to caching.
-//    The hourly/daily action rules picking this up would work as is?  
+//    The hourly/daily action rules picking this up would work as is?
     private List<ScheduledTask> scheduledTasks;
+
+    private List<ScheduledTask> scheduledTasksHistory; // might just be EventLogger to record changes here?
 }
--- src/main/java/uk/gov/ons/ssdc/caseprocessor/enrichment/ScheduledTask.java
+++ src/main/java/uk/gov/ons/ssdc/caseprocessor/enrichment/ScheduledTask.java
@@ -16,5 +16,19 @@ public class ScheduledTask {
     Would this be just for cases?  I think so..
     Would GIN indexes make this long term viable?
     Imagine several million cases of various surveys, and selecting through each one every time?
+*/
 
+/*
+    Also these scheduled tasks would be exposed to the frontend/api?
+    Would we allow the frontend/api/msg to update the scheduledTasks?  Yes - but how to stop automatic update?
+    This gets tricky
+    A user could suspend/delete/mark a scheduled task for a holiday
+    But if there was plausibly a survey wide scheduling update (say moving to every 2 months) - then all the schedule Task
+    lists would be regenerated?
+*/
+
+/*
+    With MI we could outsource entirely this scheduling to the business?
+    You tell us what to run, when and for what?  Pass us a scheduled task  list per case or collectionExercise?
+    We just process these
  */
