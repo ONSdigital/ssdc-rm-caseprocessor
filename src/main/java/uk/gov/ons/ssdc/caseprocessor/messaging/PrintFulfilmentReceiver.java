ae4ad3c37631070a38981875ba5140bfce38593a
AdamHawtin <adam.hawtin@ons.gov.uk>
2022-02-14 12:58:04 +0000

    Check for duplicate message IDs from print fulfilments

--- src/main/java/uk/gov/ons/ssdc/caseprocessor/messaging/PrintFulfilmentReceiver.java
+++ src/main/java/uk/gov/ons/ssdc/caseprocessor/messaging/PrintFulfilmentReceiver.java
@@ -1,10 +1,14 @@
 package uk.gov.ons.ssdc.caseprocessor.messaging;
 
+import static java.lang.Thread.sleep;
 import static uk.gov.ons.ssdc.caseprocessor.utils.JsonHelper.convertJsonBytesToEvent;
 
+import com.godaddy.logging.Logger;
+import com.godaddy.logging.LoggerFactory;
 import org.springframework.integration.annotation.MessageEndpoint;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.messaging.Message;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 import uk.gov.ons.ssdc.caseprocessor.logging.EventLogger;
 import uk.gov.ons.ssdc.caseprocessor.model.dto.EventDTO;
@@ -22,6 +26,7 @@ public class PrintFulfilmentReceiver {
   private final CaseService caseService;
   private final EventLogger eventLogger;
   private final FulfilmentToProcessRepository fulfilmentToProcessRepository;
+  private static final Logger log = LoggerFactory.getLogger(PrintFulfilmentReceiver.class);
 
   public PrintFulfilmentReceiver(
       CaseService caseService,
@@ -36,7 +41,22 @@ public class PrintFulfilmentReceiver {
   @ServiceActivator(inputChannel = "printFulfilmentInputChannel", adviceChain = "retryAdvice")
   public void receiveMessage(Message<byte[]> message) {
     EventDTO event = convertJsonBytesToEvent(message.getPayload());
+    log.with("correlationId", event.getHeader().getCorrelationId())
+        .info("Processing Print Fulfilment");
 
+    if (fulfilmentToProcessRepository.existsByMessageId(event.getHeader().getMessageId())) {
+      log.with("correlationId", event.getHeader().getCorrelationId())
+          .with("messageId", event.getHeader().getMessageId())
+          .info(
+              "Received duplicate fulfilment message ID, ignoring and acking the duplicate message");
+      return;
+    }
+
+    try {
+      sleep(5000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
     Case caze = caseService.getCase(event.getPayload().getPrintFulfilment().getCaseId());
 
     ExportFileTemplate exportFileTemplate =
@@ -46,6 +66,7 @@ public class PrintFulfilmentReceiver {
     fulfilmentToProcess.setExportFileTemplate(exportFileTemplate);
     fulfilmentToProcess.setCaze(caze);
     fulfilmentToProcess.setCorrelationId(event.getHeader().getCorrelationId());
+    fulfilmentToProcess.setMessageId(event.getHeader().getMessageId());
     fulfilmentToProcess.setOriginatingUser(event.getHeader().getOriginatingUser());
     fulfilmentToProcess.setUacMetadata(event.getPayload().getPrintFulfilment().getUacMetadata());
     fulfilmentToProcess.setPersonalisation(
