308de950ff245ae5d00f756d2dff401a5ad23b79
LukeLoze1 <Luke.Loze@ext.ons.gov.uk>
2022-03-10 09:15:37 +0000

    add tests

--- src/main/java/uk/gov/ons/ssdc/caseprocessor/messaging/PrintFulfilmentReceiver.java
+++ src/main/java/uk/gov/ons/ssdc/caseprocessor/messaging/PrintFulfilmentReceiver.java
@@ -1,6 +1,5 @@
 package uk.gov.ons.ssdc.caseprocessor.messaging;
 
-import static java.lang.Thread.sleep;
 import static uk.gov.ons.ssdc.caseprocessor.utils.JsonHelper.convertJsonBytesToEvent;
 
 import com.godaddy.logging.Logger;
@@ -8,7 +7,6 @@ import com.godaddy.logging.LoggerFactory;
 import org.springframework.integration.annotation.MessageEndpoint;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.messaging.Message;
-import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 import uk.gov.ons.ssdc.caseprocessor.logging.EventLogger;
 import uk.gov.ons.ssdc.caseprocessor.model.dto.EventDTO;
@@ -41,8 +39,6 @@ public class PrintFulfilmentReceiver {
   @ServiceActivator(inputChannel = "printFulfilmentInputChannel", adviceChain = "retryAdvice")
   public void receiveMessage(Message<byte[]> message) {
     EventDTO event = convertJsonBytesToEvent(message.getPayload());
-    log.with("correlationId", event.getHeader().getCorrelationId())
-        .info("Processing Print Fulfilment");
 
     if (fulfilmentToProcessRepository.existsByMessageId(event.getHeader().getMessageId())) {
       log.with("correlationId", event.getHeader().getCorrelationId())
