5f5824a5c08e112bee4b51a0c6ce7492e9070475
Hugh Brace <hugh.ons@gmail.com>
2022-04-28 15:40:18 +0100

    Unit test for scheduled task scheduler

--- src/test/java/uk/gov/ons/ssdc/caseprocessor/schedule/ScheduledTaskSchedulerTest.java
+++ src/test/java/uk/gov/ons/ssdc/caseprocessor/schedule/ScheduledTaskSchedulerTest.java
@@ -1,5 +1,35 @@
 package uk.gov.ons.ssdc.caseprocessor.schedule;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-class ScheduledTaskSchedulerTest {}
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ScheduledTaskSchedulerTest {
+
+  @Mock
+  private ClusterLeaderManager clusterLeaderManager;
+  @Mock
+  private ScheduledTaskTriggerer scheduledTaskTriggerer;
+
+  @InjectMocks ScheduledTaskScheduler underTest;
+
+  @Test
+  public void testSchedulerSchedules () {
+    // Given
+    when(clusterLeaderManager.isThisHostClusterLeader()).thenReturn(true);
+
+    // When
+    underTest.triggerScheduledTasks();
+
+    // Then
+    verify(scheduledTaskTriggerer, times(1)).triggerScheduledTasks();
+  }
+}
