1fe6217accf75cc95082475f5ed5497bed4a2a07
Hugh Brace <hugh.ons@gmail.com>
2022-04-28 12:27:14 +0100

    Check export file in IT

--- src/test/java/uk/gov/ons/ssdc/caseprocessor/messaging/NewCaseReceiverIT.java
+++ src/test/java/uk/gov/ons/ssdc/caseprocessor/messaging/NewCaseReceiverIT.java
@@ -6,6 +6,7 @@ import static uk.gov.ons.ssdc.caseprocessor.testutils.TestConstants.NEW_CASE_TOP
 import static uk.gov.ons.ssdc.caseprocessor.testutils.TestConstants.OUTBOUND_CASE_SUBSCRIPTION;
 import static uk.gov.ons.ssdc.caseprocessor.utils.Constants.OUTBOUND_EVENT_SCHEMA_VERSION;
 
+import java.time.OffsetDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -30,7 +31,9 @@ import uk.gov.ons.ssdc.caseprocessor.model.dto.NewCase;
 import uk.gov.ons.ssdc.caseprocessor.model.dto.PayloadDTO;
 import uk.gov.ons.ssdc.caseprocessor.model.repository.CaseRepository;
 import uk.gov.ons.ssdc.caseprocessor.model.repository.EventRepository;
+import uk.gov.ons.ssdc.caseprocessor.model.repository.ExportFileRowRepository;
 import uk.gov.ons.ssdc.caseprocessor.model.repository.ExportFileTemplateRepository;
+import uk.gov.ons.ssdc.caseprocessor.model.repository.FulfilmentNextTriggerRepository;
 import uk.gov.ons.ssdc.caseprocessor.model.repository.FulfilmentSurveyExportFileTemplateRepository;
 import uk.gov.ons.ssdc.caseprocessor.model.repository.ScheduledTaskRepository;
 import uk.gov.ons.ssdc.caseprocessor.model.repository.SurveyRepository;
@@ -42,31 +45,52 @@ import uk.gov.ons.ssdc.common.model.entity.Case;
 import uk.gov.ons.ssdc.common.model.entity.CollectionExercise;
 import uk.gov.ons.ssdc.common.model.entity.Event;
 import uk.gov.ons.ssdc.common.model.entity.EventType;
+import uk.gov.ons.ssdc.common.model.entity.ExportFileRow;
 import uk.gov.ons.ssdc.common.model.entity.ExportFileTemplate;
+import uk.gov.ons.ssdc.common.model.entity.FulfilmentNextTrigger;
 import uk.gov.ons.ssdc.common.model.entity.FulfilmentSurveyExportFileTemplate;
 import uk.gov.ons.ssdc.common.model.entity.Survey;
+import uk.gov.ons.ssdc.common.validation.ColumnValidator;
+import uk.gov.ons.ssdc.common.validation.MandatoryRule;
+import uk.gov.ons.ssdc.common.validation.Rule;
 
 @ContextConfiguration
 @ActiveProfiles("test")
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 public class NewCaseReceiverIT {
+
   private static final UUID TEST_CASE_ID = UUID.randomUUID();
+  private static final String TEST_PACK_CODE = "Test-Pack-Code";
 
   @Value("${queueconfig.case-update-topic}")
   private String caseUpdateTopic;
 
-  @Autowired private PubsubHelper pubsubHelper;
-  @Autowired private DeleteDataHelper deleteDataHelper;
-  @Autowired private JunkDataHelper junkDataHelper;
+  @Autowired
+  private PubsubHelper pubsubHelper;
+  @Autowired
+  private DeleteDataHelper deleteDataHelper;
+  @Autowired
+  private JunkDataHelper junkDataHelper;
 
-  @Autowired private EventRepository eventRepository;
-  @Autowired private CaseRepository caseRepository;
+  @Autowired
+  private EventRepository eventRepository;
+  @Autowired
+  private CaseRepository caseRepository;
 
-  @Autowired private ScheduledTaskRepository scheduledTaskRepository;
-  @Autowired private SurveyRepository surveyRepository;
+  @Autowired
+  private ScheduledTaskRepository scheduledTaskRepository;
+  @Autowired
+  private SurveyRepository surveyRepository;
 
-  @Autowired private ExportFileTemplateRepository exportFileTemplateRepository;
+  @Autowired
+  private ExportFileTemplateRepository exportFileTemplateRepository;
+
+  @Autowired
+  private ExportFileRowRepository exportFileRowRepository;
+
+  @Autowired
+  private FulfilmentNextTriggerRepository fulfilmentNextTriggerRepository;
 
   @Autowired
   private FulfilmentSurveyExportFileTemplateRepository fulfilmentSurveyExportFileTemplateRepository;
@@ -149,8 +173,8 @@ public class NewCaseReceiverIT {
       CollectionExercise collectionExercise = junkDataHelper.setupJunkCollex();
       // Setting up schedules
 
-      ExportFileTemplate exportFileTemplate = addExportFileTemplate("Test-Pack-Code");
-      allowPackCodeOnSurvey("Test-Pack-Code", collectionExercise.getSurvey(), exportFileTemplate);
+      ExportFileTemplate exportFileTemplate = addExportFileTemplate(TEST_PACK_CODE);
+      allowPackCodeOnSurvey(TEST_PACK_CODE, collectionExercise.getSurvey(), exportFileTemplate);
 
       ScheduleTemplate scheduleTemplate =
           createOneTaskSimpleScheduleTemplate(ChronoUnit.SECONDS, 0);
@@ -159,10 +183,18 @@ public class NewCaseReceiverIT {
 
       survey.setScheduleTemplate(scheduleTemplate);
 
+      survey.setSampleValidationRules(
+          new ColumnValidator[]{
+              new ColumnValidator("ADDRESS_LINE1", false, new Rule[]{new MandatoryRule()}),
+              new ColumnValidator("POSTCODE", false, new Rule[]{new MandatoryRule()}),
+              new ColumnValidator("SensitiveJunk", true, new Rule[] {new MandatoryRule()})
+          });
+
       surveyRepository.saveAndFlush(survey);
 
       Map<String, String> sample = new HashMap<>();
-      sample.put("Junk", "YesYouCan");
+      sample.put("ADDRESS_LINE1", "666 Fake Street");
+      sample.put("POSTCODE", "PO57 C0D");
 
       Map<String, String> sampleSensitive = new HashMap<>();
       sampleSensitive.put("SensitiveJunk", "02071234567");
@@ -217,6 +249,23 @@ public class NewCaseReceiverIT {
 
       Thread.sleep(5000);
       assertThat(scheduledTaskRepository.findAll().size()).isEqualTo(0);
+
+      FulfilmentNextTrigger fulfilmentNextTrigger = new FulfilmentNextTrigger();
+      fulfilmentNextTrigger.setId(UUID.randomUUID());
+      fulfilmentNextTrigger.setTriggerDateTime(OffsetDateTime.now());
+      fulfilmentNextTriggerRepository.saveAndFlush(fulfilmentNextTrigger);
+
+      Thread.sleep(5000);
+
+      List<ExportFileRow> exportFileRows = exportFileRowRepository.findAll();
+      ExportFileRow exportFileRow = exportFileRows.get(0);
+
+      // Then
+      Assertions.assertThat(exportFileRow).isNotNull();
+      Assertions.assertThat(exportFileRow.getBatchQuantity()).isEqualTo(1);
+      Assertions.assertThat(exportFileRow.getPackCode()).isEqualTo(TEST_PACK_CODE);
+      Assertions.assertThat(exportFileRow.getExportFileDestination()).isEqualTo("SUPPLIER_A");
+      Assertions.assertThat(exportFileRow.getRow()).isEqualTo("\"666 Fake Street\"|\"PO57 C0D\"");
     }
   }
 
@@ -234,7 +283,7 @@ public class NewCaseReceiverIT {
   private ExportFileTemplate addExportFileTemplate(String packCode) {
     ExportFileTemplate exportFileTemplate = new ExportFileTemplate();
     exportFileTemplate.setPackCode(packCode);
-    exportFileTemplate.setTemplate(new String[] {"ADDRESS_LINE1", "POSTCODE"});
+    exportFileTemplate.setTemplate(new String[]{"ADDRESS_LINE1", "POSTCODE"});
     exportFileTemplate.setExportFileDestination("SUPPLIER_A");
     exportFileTemplate.setMetadata("{\"foo\": \"bar\"}");
     exportFileTemplate.setDescription("test");
