b717361c4c22cc6a79357fc5053167ad56199146
LukeLoze1 <44841627+LukeLoze1@users.noreply.github.com>
2022-10-10 15:58:44 +0100

    82 remove rasrm integration bits (#168)

--- src/main/java/uk/gov/ons/ssdc/caseprocessor/rasrm/model/dto/RasRmCaseIacResponseDTO.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.ons.ssdc.caseprocessor.rasrm.model.dto;
-
-import lombok.Data;
-
-@Data
-public class RasRmCaseIacResponseDTO {
-  private String iac;
-}
--- src/main/java/uk/gov/ons/ssdc/caseprocessor/schedule/CaseClassifier.java
+++ src/main/java/uk/gov/ons/ssdc/caseprocessor/schedule/CaseClassifier.java
@@ -1,39 +1,20 @@
 package uk.gov.ons.ssdc.caseprocessor.schedule;
 
-import static uk.gov.ons.ssdc.caseprocessor.rasrm.constants.RasRmConstants.BUSINESS_SAMPLE_DEFINITION_URL_SUFFIX;
-
 import java.util.UUID;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
-import uk.gov.ons.ssdc.caseprocessor.rasrm.service.RasRmSampleSummaryCollexLinkService;
 import uk.gov.ons.ssdc.common.model.entity.ActionRule;
 
 @Component
 public class CaseClassifier {
   private final JdbcTemplate jdbcTemplate;
-  private final RasRmSampleSummaryCollexLinkService rasRmSampleSummaryCollexLinkService;
 
-  public CaseClassifier(
-      JdbcTemplate jdbcTemplate,
-      RasRmSampleSummaryCollexLinkService rasRmSampleSummaryCollexLinkService) {
+  public CaseClassifier(JdbcTemplate jdbcTemplate) {
     this.jdbcTemplate = jdbcTemplate;
-    this.rasRmSampleSummaryCollexLinkService = rasRmSampleSummaryCollexLinkService;
   }
 
   public void enqueueCasesForActionRule(ActionRule actionRule) {
-    if (actionRule
-        .getCollectionExercise()
-        .getSurvey()
-        .getSampleDefinitionUrl()
-        .endsWith(BUSINESS_SAMPLE_DEFINITION_URL_SUFFIX)) {
-      // This only needs to be done once, for efficiency, but it's pretty horrible having to hack
-      // it in right here. In the ideal world, there would be a more elegant place to put this
-      // in the code... but its horribleness is largely due to how difficult RAS-RM APIs are
-      rasRmSampleSummaryCollexLinkService.linkSampleSummaryToCollex(
-          actionRule.getCollectionExercise());
-    }
-
     UUID batchId = UUID.randomUUID();
 
     jdbcTemplate.update(
