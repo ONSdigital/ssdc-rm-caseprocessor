c50d47b0897cf360db8bc094401aa6d4dfc553ec
Daniel Banks_ONS <daniel.banks@ons.gov.uk>
2024-01-18 11:30:29 +0000

    fixed emit topic generating wrong json

--- python_spike/caseprocessor/dto/payload_dto.py
+++ python_spike/caseprocessor/dto/payload_dto.py
@@ -1,12 +1,12 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from .newCase import NewCase
 from .case_update_dto import CaseUpdateDTO
-from dataclasses_json import LetterCase, dataclass_json
+from dataclasses_json import LetterCase, dataclass_json, config
 from typing import Optional
 
 
 @dataclass_json(letter_case=LetterCase.CAMEL)
 @dataclass
 class PayloadDTO:
-    new_case: Optional[NewCase] = None
-    case_update_dto: Optional[CaseUpdateDTO] = None
+    new_case: Optional[NewCase] = field(default=None, metadata=config(exclude=lambda x: x is None))
+    case_update: Optional[CaseUpdateDTO] = None
