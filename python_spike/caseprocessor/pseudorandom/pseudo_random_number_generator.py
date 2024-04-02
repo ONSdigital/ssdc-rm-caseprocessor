c903c61eb8ab156cf8fdf055d1c89a1eb12e153e
Daniel Banks_ONS <daniel.banks@ons.gov.uk>
2024-04-02 11:52:38 +0100

    removed class pseudorandom number generator

--- python_spike/caseprocessor/pseudorandom/pseudo_random_number_generator.py
+++ python_spike/caseprocessor/pseudorandom/pseudo_random_number_generator.py
@@ -1,51 +1,46 @@
-import logging
-
 from .NumberTheory import factor
 from .fpe_encryptor import FPEEncryptor
+from config import CaseRegGeneratorConfig
+
+"""
+ Format Preserving 'Encryption' using the scheme FE1 from the paper "Format-Preserving Encryption"
+ by Bellare, Rogaway, et al (http://eprint.iacr.org/2009/251).
+ """
+
+LOWEST_SAFE_NUMBER_OF_ROUNDS = 3
+
+modulus = CaseRegGeneratorConfig.HIGHEST_POSSIBLE_CASE_REF - CaseRegGeneratorConfig.LOWEST_POSSIBLE_CASE_REF
+factors = factor(modulus)
+first_factor = factors[0]
+second_factor = factors[1]
 
 
-class PseudorandomNumberGenerator:
+def get_pseudorandom(original_number: int, key: bytes) -> int:
     """
-     Format Preserving 'Encryption' using the scheme FE1 from the paper "Format-Preserving Encryption"
-     by Bellare, Rogaway, et al (http://eprint.iacr.org/2009/251).
-     """
+    Generic Z_n FPE 'encryption' using the FE1 scheme.
 
-    LOWEST_SAFE_NUMBER_OF_ROUNDS = 3
+    :param original_number: The number to 'encrypt'. Must not be null.
+    :param key: Secret key to 'salt' the hashing with.
+    :return: the encrypted version of <code>originalNumber</code>.
+    :raises ValueError: if any of the parameters are invalid.
+    """
+    if original_number > modulus:
+        raise ValueError("Cannot encrypt a number bigger than the modulus "
+                         "(otherwise this wouldn't be format preserving encryption")
 
-    def __init__(self, modulus):
-        self.modulus = modulus
-        self.factors = factor(modulus)
-        self.first_factor = self.factors[0]
-        self.second_factor = self.factors[1]
+    encryptor = FPEEncryptor(key, modulus)
 
-    def get_pseudorandom(self, original_number: int, key: bytes) -> int:
-        """
-        Generic Z_n FPE 'encryption' using the FE1 scheme.
+    pseudo_random_number = original_number
 
-        :param original_number: The number to 'encrypt'. Must not be null.
-        :param key: Secret key to 'salt' the hashing with.
-        :return: the encrypted version of <code>originalNumber</code>.
-        :raises ValueError: if any of the parameters are invalid.
-        """
-        if original_number > self.modulus:
-            raise ValueError("Cannot encrypt a number bigger than the modulus "
-                             "(otherwise this wouldn't be format preserving encryption")
+    # Apply the same algorithm repeatedly on x for the number of rounds given by getNumberOfRounds.
+    # Each round increases the security.
+    # Note that the attribute and method names used align to the paper on FE1, not Python conventions on readability
+    for i in range(LOWEST_SAFE_NUMBER_OF_ROUNDS):
+        # Split the value of x in to left and right values (think splitting the binary in to two halves),
+        # around the second (smaller) factor
+        left = int(pseudo_random_number / second_factor)
+        right = int(pseudo_random_number % second_factor)
 
-        encryptor = FPEEncryptor(key, self.modulus)
-
-        pseudo_random_number = original_number
-
-        # Apply the same algorithm repeatedly on x for the number of rounds given by getNumberOfRounds.
-        # Each round increases the security.
-        # Note that the attribute and method names used align to the paper on FE1, not Python conventions on readability
-        for i in range(self.LOWEST_SAFE_NUMBER_OF_ROUNDS):
-            # Split the value of x in to left and right values (think splitting the binary in to two halves),
-            # around the second (smaller) factor
-            left = int(pseudo_random_number / self.second_factor)
-            right = int(pseudo_random_number % self.second_factor)
-
-            print(f"Left = {left}   Right = {right}")
-
-            w = int((left + encryptor.one_way_function(i, right)) % self.first_factor)
-            pseudo_random_number = self.first_factor * right + w
-        return pseudo_random_number
+        w = int((left + encryptor.one_way_function(i, right)) % first_factor)
+        pseudo_random_number = first_factor * right + w
+    return pseudo_random_number
