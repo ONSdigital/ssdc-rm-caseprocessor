dba1de0e63d190ec0633e2d280a0c4a6e63a26d5
Ryan Grundy <ryan.grundy@ons.gov.uk>
2024-11-20 15:29:48 +0000

    Attempt at setup

---
+++ python_spike/setup.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+import setuptools
+
+setuptools.setup(
+    name="caseprocessor",
+    version="0.9.7",
+    description="Caseprocessor",
+    url="https://github.com/ONSdigital/ssdc-rm-caseprocessor",
+    packages=setuptools.find_packages(),
+)
